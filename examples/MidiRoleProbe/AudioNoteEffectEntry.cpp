402a1335e538ade30d7dba0ca84c82fdc7335653
001null100 <128686360+001null100@users.noreply.github.com>
2026-08-29 19:04:02 +0300

    Wire CLAP-preferred audio probe variant

--- examples/MidiRoleProbe/AudioNoteEffectEntry.cpp
+++ examples/MidiRoleProbe/AudioNoteEffectEntry.cpp
@@ -1,5 +1,5 @@
 #include "ProbePlugin.hpp"
 #include "RoleDefinitions.hpp"
 #include <nullclap/Entry.hpp>
-using Probe = nullclap::midi_role_probe::ProbePlugin<nullclap::midi_role_probe::audioNoteEffectId, nullclap::midi_role_probe::audioNoteEffectName, nullclap::midi_role_probe::audioNoteEffectFeatures>;
+using Probe = nullclap::midi_role_probe::ProbePlugin<nullclap::midi_role_probe::AudioEffectClapPreferredRole>;
 NULLCLAP_DEFINE_ENTRY(Probe);
