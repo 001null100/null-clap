9eae81c6633b8ed47ecc688a7f686eb7d179082b
001null100 <128686360+001null100@users.noreply.github.com>
2026-08-29 19:04:14 +0300

    Wire NIH-style audio probe variant

--- examples/MidiRoleProbe/InstrumentEntry.cpp
+++ examples/MidiRoleProbe/InstrumentEntry.cpp
@@ -1,5 +1,5 @@
 #include "ProbePlugin.hpp"
 #include "RoleDefinitions.hpp"
 #include <nullclap/Entry.hpp>
-using Probe = nullclap::midi_role_probe::ProbePlugin<nullclap::midi_role_probe::instrumentId, nullclap::midi_role_probe::instrumentName, nullclap::midi_role_probe::instrumentFeatures>;
+using Probe = nullclap::midi_role_probe::ProbePlugin<nullclap::midi_role_probe::AudioEffectNihPortRole>;
 NULLCLAP_DEFINE_ENTRY(Probe);
